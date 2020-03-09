efaa787ca598e8e505f3b1c82e9aebcc93fec860
XY Xu <michaelxysg@gmail.com>
2020-03-09 09:35:34 +0800

    utils for saving and loading models

---
+++ utlis.py
@@ -0,0 +1,30 @@
+import torch
+
+def save_checkpoint(model_class, model, filepath):
+    checkpoint = create_checkpoint(model_class, model)
+    torch.save(checkpoint, filepath)
+    return
+
+def create_checkpoint(model_class, model, optimizer = None):
+    checkpoint = {'model_class': model_class,
+          'state_dict': model.state_dict(),}
+    if optimizer:
+          checkpoint['optimizer'] : optimizer.state_dict()
+    return checkpoint
+
+def load_checkpoint(filepath):
+    checkpoint = torch.load(filepath)
+    model = checkpoint['model_class']
+    model.load_state_dict(checkpoint['state_dict'])
+    for parameter in model.parameters():
+        parameter.requires_grad = False
+
+    model.eval()
+    return model
+
+def create_dir(directory):
+    try:
+        os.mkdir(directory)
+    except OSError:
+        pass
+    return
